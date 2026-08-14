2574b9d2ef043a2ffa06984b00892a756a45fb3d
mohamed-12-abdelkader <mharedy1212@gmail.com>
2026-08-14 12:37:33 +0300

    ghhhk

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL ??
-  'http://localhost:8000/';
+  'https://mohamed-saber-api.next-edu.online/';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
